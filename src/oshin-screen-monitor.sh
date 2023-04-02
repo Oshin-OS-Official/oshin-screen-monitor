ad1743a6f0fb5f2419c81fbad8d6ec67b814bae3
Prharsha G <gsvprharsha@gmail.com>
2023-04-02 15:44:04 +0530

    Add files via upload

---
+++ src/oshin-screen-monitor.sh
@@ -0,0 +1,13 @@
+# Maintainer: Team Oshin OS (https://github.com/Oshin-OS)
+
+#!/bin/sh
+
+while true
+
+do
+
+sleep 7200
+
+notify-send --urgency=critical --app-name="Oshin Screen Monitor" "2 Hours have passed. Please Take Rest else it might effect your eyes :)" -i /usr/share/oshin-welcome/img/oshin-welcome.png
+
+done
